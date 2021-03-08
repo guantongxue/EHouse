beee5a33fce00199ed35ab7ee558759882c44c71
guantongxue <939730442@qq.com>
2021-03-08 13:45:22 +0800

    注册页面填写

---
+++ src/components/phoneVerification.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="phone-content">
+    <a-form :label-col="labelCol" :wrapper-col="wrapperCol">
+      <a-form-item
+        label="手机号码"
+        has-feedback
+        :validate-status="phone_status"
+        :required="true"
+      >
+        <a-input
+          v-model="form.telphone"
+          id="success"
+          placeholder="请输入手机号码"
+        />
+      </a-form-item>
+        <a-form-item
+        label="验证码"
+        has-feedback
+        :validate-status="phone_status"
+        :required="true"
+      >
+        <a-input
+          v-model="form.verificationCode"
+          id="success"
+          class="input-adst"
+          placeholder="请输入验证码"
+        />
+        <a-button type="danger" class="button-adst">
+          获取验证码
+        </a-button>
+      </a-form-item>
+      <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
+        <a-button type="primary" html-type="submit" :size="'large'" @click="submitTelInfo">
+                    立即绑定
+        </a-button>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "phoneVerification",
+  data() {
+    return {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 5 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 12 },
+      },
+      form:{
+          telphone:"",
+          verificationCode:""
+      },
+      phone_status:""
+    };
+  },
+  props: {
+
+  },
+  methods: {
+  submitTelInfo(){
+
+  }
+  },
+};
+</script>
+<style  scoped>
+.phone-content {
+  width: 60%;
+  margin-left: 18%;
+}
+.input-adst{
+    width: 60%;
+}
+.button-adst{
+  /* position: absolute; */
+  right: 0;
+}
+</style>
--- src/views/register.vue
+++ src/views/register.vue
@@ -2,25 +2,28 @@
   <div>
     <header-register></header-register>
     <a-steps v-model="current" type="navigation" :style="stepStyle">
-      <a-step status="finish" title="用户信息填写" >
+      <a-step status="finish" title="用户信息填写" @click="changeCurrent(0)" >
         <a-icon slot="icon" type="user" />
       </a-step>
-      <a-step status="process" title="绑定手机">
+      <a-step status="process" title="绑定手机" @click="changeCurrent(1)">
         <a-icon slot="icon" type="mobile" />
       </a-step>
     </a-steps>
-    <write-user-info @submitUserInfo="submitUserInfo"></write-user-info>
+    <write-user-info v-if="current==0" @submitUserInfo="submitUserInfo"></write-user-info>
+    <phone-verification v-if="current==1"></phone-verification>
   </div>
 </template>
 
 <script>
 import HeaderRegister from '../components/headerRegister.vue';
 import WriteUserInfo from '../components/writeUserInfo.vue';
+import PhoneVerification from '../components/phoneVerification.vue';
+
 export default {
   name:'register',
   data(){
     return{
-      current: 1,
+      current: 0,
       stepStyle: {
         marginBottom: '60px',
         boxShadow: '0px -1px 0 0 #e8e8e8 inset',
@@ -29,11 +32,15 @@ export default {
   },
   components:{
     HeaderRegister,
-    WriteUserInfo
+    WriteUserInfo,
+    PhoneVerification
   },
   methods:{
     submitUserInfo(form){
       console.log("用户信息",form)
+    },
+    changeCurrent(index){
+      this.current = index
     }
   }
 };
